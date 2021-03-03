ab2374fa1276713794230c0ced682eb898810dd4
kevin_p <kprsd14@gmail.com>
2021-03-03 15:25:08 -0500

    Added JS and desktop media queries

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const form = document.querySelector('.email-submit');
-const button = document.querySelector('.submit-btn');
+const input = document.querySelector('.email-input');
+const errorMsg = document.querySelector('.error-message');
+
+
+form.addEventListener('submit', e => {
+
+  // Email validation regular expression
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if(input.value.match(regex)) {
+    alert('Email Successfully sent!');
+
+  } else {
+    
+    // Prevent form submit
+    e.preventDefault();
+
+    // Show error message
+    errorMsg.classList.add('show');
+    
+    // Make input border red
+    input.classList.add('error');
+  }
+})
+
+
